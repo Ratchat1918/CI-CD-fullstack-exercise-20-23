10081298db41a15a4c7459400b9a706ec08e26a2
ratchat1918 <eusenko101@gmail.com>
2026-07-30 19:55:52 +0300

    got rid of extra repository

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+app.use(cors({
+  origin: 'http://localhost:5173'
+}))
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/api/nasa', async (req, res) => {
+    try {
+        const webResponse = await fetch('https://images-api.nasa.gov/search?q=air%20show&media_type=image&year_start=2020&year_end=2026');
+        const data = await webResponse.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching data from NASA API:', error);
+        res.status(500).json({ error: 'Failed to fetch data from NASA API' });
+    }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}/api/nasa`);
+});
--- nasa-api/b2btests/page.test.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('should load the NASA images page and display images', async ({ page }) => {
-  await page.goto('http://localhost:5173/')
-  await page.waitForSelector('.image-container img')
-  const images = await page.$$('.image-container img')
-  expect(images.length).toBeGreaterThan(0)
-})
