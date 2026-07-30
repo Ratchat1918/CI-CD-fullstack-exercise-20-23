e95fd982bc1f7d460d4a86fb6e23425fc1afa9d1
ratchat1918 <eusenko101@gmail.com>
2026-07-30 19:41:27 +0300

    main pipeline initial commit

--- nasa-api/app.js
+++ nasa-api/app.js
@@ -15,17 +15,17 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
-app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}/api/nasa`);
-});
-
 app.get('/api/nasa', async (req, res) => {
-    const apiKey = process.env.NASA_API_KEY;
     try {
         const webResponse = await fetch('https://images-api.nasa.gov/search?q=air%20show&media_type=image&year_start=2020&year_end=2026');
         const data = await webResponse.json();
         res.json(data);
-    }catch (error) {
+    } catch (error) {
         console.error('Error fetching data from NASA API:', error);
         res.status(500).json({ error: 'Failed to fetch data from NASA API' });
-    }});
+    }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}/api/nasa`);
+});
